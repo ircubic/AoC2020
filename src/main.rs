df3181e95991403b8207715b362dbaa6bbfb29df
Daniel Bruce <ircubic@gmail.com>
2020-12-02 22:41:12 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -24,16 +24,28 @@ fn read_numbers_from_lines<P>(filename: P) -> io::Result<impl Iterator<Item=isiz
   }))
 }
 
-fn aoc01_2(path: &Path) -> isize {
+fn aoc01_1(path: &Path) -> usize {
   let mut numbers = HashSet::<isize>::new();
 
   for num in read_numbers_from_lines(path).unwrap() {
-    if numbers.len() < 2 { continue; }
+    let target = 2020 - num;
+    if numbers.contains(&target) {
+      return (target * num) as usize;
+    }
+    numbers.insert(num);
+  }
 
+  panic!("Unable to find a solution")
+}
+
+fn aoc01_2(path: &Path) -> usize {
+  let mut numbers = HashSet::<isize>::new();
+
+  for num in read_numbers_from_lines(path).unwrap() {
     for other_num in numbers.iter() {
       let target = 2020 - other_num - num;
       if target != *other_num && target != num && numbers.contains(&target) {
-        return target * other_num * num;
+        return (target * other_num * num) as usize;
       }
     }
 
@@ -43,22 +55,62 @@ fn aoc01_2(path: &Path) -> isize {
   panic!("Unable to find a solution")
 }
 
-fn aoc01_1(path: &Path) -> isize {
-  let mut numbers = HashSet::<isize>::new();
+#[derive(Debug)]
+struct AoC02PasswordEntry {
+  min: i32,
+  max: i32,
+  character: char,
+  password: Vec<char>,
+}
 
-  for num in read_numbers_from_lines(path).unwrap() {
-    let target = 2020 - num;
-    if numbers.contains(&target) {
-      return target * num;
-    }
-    numbers.insert(num);
+impl AoC02PasswordEntry {
+  fn from_line(line: &str) -> Self
+  {
+    let split = line.split(&['-', ' ', ':'][..]).filter(|x| x.len() > 0).collect::<Vec<_>>();
+    let min = split[0].parse::<i32>().expect("Unable to parse min from string") - 1;
+    let max = split[1].parse::<i32>().expect("Unable to parse max from string") - 1;
+    let character = split[2].chars().nth(0).expect("Unable to parse char from string");
+    AoC02PasswordEntry { min, max, character, password: split[3].chars().collect() }
   }
 
-  panic!("Unable to find a solution")
+  fn validate_01(&self) -> bool
+  {
+    let amount = self.password.iter().filter(|&&c| c == self.character).count() as i32;
+    amount >= self.min && amount <= self.max
+  }
+
+  fn validate_02(&self) -> bool
+  {
+    if self.max >= self.password.len() as i32 {
+      return false
+    }
+    [self.min, self.max].iter()
+      .filter(|&&n| self.password[n as usize] == self.character)
+      .count() == 1
+  }
 }
+
+fn aoc02_1(path: &Path) -> usize {
+  read_lines(path).unwrap()
+    .filter(|l|
+      AoC02PasswordEntry::from_line(&l.as_ref().unwrap()).validate_01()
+    )
+    .count()
+}
+
+fn aoc02_2(path: &Path) -> usize {
+  read_lines(path).unwrap()
+    .filter(|l|
+      AoC02PasswordEntry::from_line(&l.as_ref().unwrap()).validate_02()
+    )
+    .count()
+}
+
 
 fn main() {
   let path = PathBuf::from(PATH);
   println!("Result of 1-1: {}", aoc01_1(&path.join(r"1-1.txt")));
   println!("Result of 1-2: {}", aoc01_2(&path.join(r"1-1.txt")));
+  println!("Result of 2-1: {}", aoc02_1(&path.join(r"2-1.txt")));
+  println!("Result of 2-2: {}", aoc02_2(&path.join(r"2-1.txt")));
 }
